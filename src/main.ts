0c4b4b4923a74622e627e2a3c8a28f6990c84427
daniejl04 <danielestebanjimenezlopez@gmail.com>
2024-07-12 06:28:31 -0500

    created services for send iframe of the user

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
 
-  console.log('Successfully connected to the database');
+  const config = new DocumentBuilder()
+    .setTitle('Iframe API')
+    .setDescription(
+      'API Iframe',
+    )
+    .setVersion('1.0')
+    .addTag('team iframe')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.setGlobalPrefix('/v1/api');
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}/v1/api`);
 }
 bootstrap();
--- src/modules/iframe/entities/iframe.entity.ts
+++ src/modules/iframe/entities/iframe.entity.ts
@@ -4,22 +4,12 @@ import { Document } from 'mongoose';
 @Schema({ timestamps: true })
 export class Iframe extends Document {
   @Prop({ required: true })
-  user_id: number;
+  api_key_user: string;
 
-  @Prop({ required: true })
-  domain: string;
+   @Prop({ required: true })
+   iframe: string;
 
-  @Prop({ required: true })
-  seniority: string;
-
-  @Prop({ required: true })
-  language: string;
-
-  @Prop()
-  color: string;
-
-  @Prop()
-  typography: string;
 }
 
 export const IframeSchema = SchemaFactory.createForClass(Iframe);
+
