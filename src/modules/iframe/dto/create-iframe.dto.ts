3920852a8bcb31b0f44183d9e24fab47ab41da0e
daniejl04 <danielestebanjimenezlopez@gmail.com>
2024-07-12 06:47:18 -0500

    isNumber

--- src/modules/iframe/dto/create-iframe.dto.ts
+++ src/modules/iframe/dto/create-iframe.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsOptional, IsString } from "@nestjs/class-validator";
+import { IsOptional, IsString } from "@nestjs/class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateIframeDto {
