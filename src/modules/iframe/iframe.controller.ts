8865f7edc83702565fad05b6225bd723c886ed01
anacaszapata <anacaszapata@gmail.com>
2024-07-25 11:47:44 -0500

    fix conflicts

--- src/modules/iframe/iframe.controller.ts
+++ src/modules/iframe/iframe.controller.ts
@@ -9,7 +9,7 @@ import {
 import { IframeService } from './iframe.service';
 import { CreateIframeDto } from './dto/create-iframe.dto';
 import { ApiKeyGuard } from 'src/common/auth/auth/auth.guard';
-import { ApiBadRequestResponse, ApiBody, ApiOperation, ApiParam, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Iframe } from './entities/iframe.entity';
 @ApiTags('iframe')
 @Controller('iframe')
